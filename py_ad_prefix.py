a3d1f17a8a6bd561f74c5c8556f90d45b58a0f52
chenyouhui1 <chenyouhui1@joyy.com>
2023-10-26 12:42:31 +0800

    add prefix

--- py_ad_prefix.py
+++ py_ad_prefix.py
@@ -1,4 +1,4 @@
-# 移除文件名前缀
+# 文件名添加前缀
 
 import os
 import argparse
@@ -8,26 +8,26 @@ parser = argparse.ArgumentParser(description='Description of your script.')
 
 # 添加命令行参数
 parser.add_argument('arg1', type=str, help='prefix string')
+parser.add_argument('arg2', type=str, help='[all] add prefix to all file, even if the file already has the prefix')
 
 # 解析命令行参数
 args = parser.parse_args()
 
 # 设置目录和前缀
-prefix = args.arg1#"stu_gameUI_"
+prefix = args.arg1
 directory = "./"
+isAll = args.arg2.casefold() == "all" 
 
-
-def remove_prefix(file_path, prefix):
+def add_prefix(file_path):
     # 获取文件名和扩展名，这个函数获取的文件名会带路径
     _filename, file_extension = os.path.splitext(file_path)
-    # 使用os.path.basename获取文件名
+    # 使用os.path.basename获取文件名包含扩展名
     filename = os.path.basename(file_path)
-    # print('file_path : ', file_path, filename,filename.startswith(prefix))
-    # 检查文件名是否以指定前缀开头
-    if filename.startswith(prefix):
-        # 构建新的文件名
-        new_filename = filename[len(prefix):] #+ file_extension
 
+    # 检查文件名是否以指定前缀开头
+    if isAll or not filename.startswith(prefix):
+        # 构建新的文件名
+        new_filename = prefix + filename
         # 构建新的完整路径
         new_path = os.path.join(os.path.dirname(file_path), new_filename)
 
@@ -35,13 +35,13 @@ def remove_prefix(file_path, prefix):
         os.rename(file_path, new_path)
         print(f"重命名文件: {file_path} ---------------> {new_path}")
 
-def batch_remove_prefix(directory, prefix):
+def batch_add_prefix(directory):
     # 遍历目录中的所有文件
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
         # 检查是否为文件而不是子目录
         if os.path.isfile(file_path):
-            remove_prefix(file_path, prefix)
+            add_prefix(file_path)
 
 # 执行批量重命名
-batch_remove_prefix(directory, prefix)
+batch_add_prefix(directory)
